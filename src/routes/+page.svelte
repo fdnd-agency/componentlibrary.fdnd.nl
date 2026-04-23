562a8486469c554facce7d80bc23cc061a1ce55e
Justus Sturkenboom <justus@ju5tu5.nl>
2026-04-23 17:06:32 +0200

    ops: Initialized storybook with some example components

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to the FDND component library</h1>
+
+<p>
+	You are seeing this page so you probably ran <code>npm run dev</code>. As this is a component
+	library there is not that much to see here. Please try one of the following options.
+</p>
+
+<ul>
+	<li>
+		<code>npm run storybook</code> to run storybook and check out your components in a visual layout
+	</li>
+</ul>

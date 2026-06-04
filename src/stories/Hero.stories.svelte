0e1d81bbb034af2658f29f0cf5c8d25443da0191
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-06-04 22:10:44 +0200

    feat: add Hero Storybook story
    Add Hero.stories.svelte with a fullscreen default story and all required content props

---
+++ src/stories/Hero.stories.svelte
@@ -0,0 +1,25 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+  import Hero from '$lib/organisms/Hero.svelte'
+
+  const { Story } = defineMeta({
+    title: 'Organisms/Hero',
+    component: Hero,
+    parameters: {
+      layout: 'fullscreen'
+    }
+  })
+</script>
+
+<Story
+  name="Default"
+  args={{
+    label: 'Associate Degree',
+    title: 'Frontend Design & Development',
+    image: '/herostudenten-image-fdnd.jpg',
+    imageAlt: 'Studenten van FDND',
+    duration: 'Twee jaar',
+    type: 'Voltijd',
+    openAvond: '9 juni 2026'
+  }}
+/>

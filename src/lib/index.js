56fd751193ba3898e486fcc03c3614b349796439
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-06-11 17:15:26 +0200

    feat: export Footer from index.js
    Adds Footer organism to the component library exports.

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,6 +6,9 @@ export { default as Button } from './atoms/Button.svelte'
 // Reexport molecules
 export { default as Header } from './molecules/Header.svelte'
 
+// Reexport organisms
+export { default as Footer } from './organisms/Footer.svelte'
+
 // Reexport templates
 export { default as Page } from './templates/Page.svelte'
 

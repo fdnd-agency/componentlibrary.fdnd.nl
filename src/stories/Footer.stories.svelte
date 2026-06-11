fa301b82d3fd27770f1d504f74e4925874ee00f0
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-06-11 17:20:32 +0200

    feat: add Footer Storybook story
    Adds Storybook story for the Footer organism under Organisms/Footer.

---
+++ src/stories/Footer.stories.svelte
@@ -0,0 +1,14 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+  import Footer from '$lib/organisms/Footer.svelte'
+
+  const { Story } = defineMeta({
+    title: 'Organisms/Footer',
+    component: Footer,
+    parameters: {
+      layout: 'fullscreen'
+    }
+  })
+</script>
+
+<Story name="Default" />

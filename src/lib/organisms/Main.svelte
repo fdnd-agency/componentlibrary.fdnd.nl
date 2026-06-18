2afe5805f8e922a05e77b4c4c5805951cd85421d
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-06-18 11:31:06 +0200

    fix: refine Main padding-bottom spacing (#59)
    The initial padding-bottom of 2rem was too tight. Increased to 3rem so page content has enough spacing

--- src/lib/organisms/Main.svelte
+++ src/lib/organisms/Main.svelte
@@ -10,7 +10,7 @@
 	.main {
 		background-color: var(--background);
 		border-radius: 0 0 1.5rem 1.5rem;
-		padding-bottom: 2rem;
+		padding-bottom: 3rem;
 		margin: 0 calc(var(--radius)) calc(var(--radius));
 	}
 </style>

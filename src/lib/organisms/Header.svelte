0cd1098b992e886e5a388b6d639fed55a042ba8b
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-06-18 16:57:14 +0200

    fix: deleted comment

--- src/lib/organisms/Header.svelte
+++ src/lib/organisms/Header.svelte
@@ -368,6 +368,7 @@
 					grid-row: 2;
 					grid-column: 2;
 					justify-content: flex-end; /* refactoring layout 1440px — nav vult kolom zodat grijze achtergrond full-width loopt */
+					justify-content: flex-end;
 					padding: 1.5rem var(--grid-1-calc) 1.5rem 1rem;
 				}
 

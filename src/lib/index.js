69dc7699627e2609400c45fda154fe19b0b80889
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-06-18 13:54:00 +0200

    feat: export Intro and CTA component

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,6 +7,8 @@ export { default as Button } from './atoms/Button.svelte'
 export { default as Header } from './organisms/Header.svelte'
 export { default as Hero } from './organisms/Hero.svelte'
 export { default as Main } from './organisms/Main.svelte'
+export { default as Intro } from './organisms/Intro.svelte'
+export { default as CTA } from './organisms/CTA.svelte'
 export { default as Footer } from './organisms/Footer.svelte'
 
 // Reexport templates
